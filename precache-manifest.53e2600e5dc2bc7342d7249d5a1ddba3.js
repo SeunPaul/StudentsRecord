79a27385bb120eeb7cce4c986c19caaea69fe7a0
SeunPaul <seunpaul148@gmail.com>
2020-04-02 01:07:50 +0100

    Updates

---
+++ precache-manifest.53e2600e5dc2bc7342d7249d5a1ddba3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a93f2d7c839866c7f4597810e573e94e",
+    "url": "/StudentsRecord/index.html"
+  },
+  {
+    "revision": "b31593b5dfc964e66fc1",
+    "url": "/StudentsRecord/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3f1cde15ba0a2c45fbba",
+    "url": "/StudentsRecord/static/css/main.16ca8134.chunk.css"
+  },
+  {
+    "revision": "b31593b5dfc964e66fc1",
+    "url": "/StudentsRecord/static/js/2.b802cac4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/StudentsRecord/static/js/2.b802cac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f1cde15ba0a2c45fbba",
+    "url": "/StudentsRecord/static/js/main.39ffbdd1.chunk.js"
+  },
+  {
+    "revision": "cb92cae3a45d9f960460",
+    "url": "/StudentsRecord/static/js/runtime-main.9bb6910c.js"
+  },
+  {
+    "revision": "5d4b90f71e318c09d9c174b3e1866a4f",
+    "url": "/StudentsRecord/static/media/homeLogo.5d4b90f7.png"
+  },
+  {
+    "revision": "e1f92cf02d3d86374ad4db004b39171b",
+    "url": "/StudentsRecord/static/media/schoolLogo.e1f92cf0.png"
+  },
+  {
+    "revision": "a2a188d04be9be60970ee107842d021e",
+    "url": "/StudentsRecord/static/media/schoolPhoto.a2a188d0.jpg"
+  },
+  {
+    "revision": "96fffeda6aac6b72615d212423b3695f",
+    "url": "/StudentsRecord/static/media/seun.96fffeda.jpg"
+  }
+]);
